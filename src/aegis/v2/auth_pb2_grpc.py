2aa0577d92603738b6d5b36041534fd8f9ad14a3
Enzo Gaggiotti <enzo.gaggiotti@epitech.eu>
2026-03-30 17:29:27 +0200

    [FIX] Restored configuration and fixed gRPC code nesting

--- src/aegis/v2/auth_pb2_grpc.py
+++ src/aegis/v2/auth_pb2_grpc.py
@@ -3,7 +3,7 @@
 import grpc
 import warnings
 
-from aegis.v2 import auth_pb2 as aegis_dot_v2_dot_auth__pb2
+from . import auth_pb2 as aegis_dot_v2_dot_auth__pb2
 
 GRPC_GENERATED_VERSION = '1.80.0'
 GRPC_VERSION = grpc.__version__
